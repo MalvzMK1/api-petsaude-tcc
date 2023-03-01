75f69890a24c05c899574d1a7a70a35bda871151
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-03-01 11:28:54 -0300

    chore: adding .editorconfig file

--- src/controller/phoneNumberController.ts
+++ src/controller/phoneNumberController.ts
@@ -1,43 +1,38 @@
 import PhoneNumberModel from '../model/phoneNumberModel';
 import Message from '../messages/message';
 import {
-    CreateUserInfosProps,
-    UpdateUserInfosProps,
+	CreateUserInfosProps,
+	UpdateUserInfosProps,
 } from '../lib/userInfosProps';
 import { VetInfos } from '@prisma/client';
 
 const message = new Message();
 const phoneNumberModel = new PhoneNumberModel();
 
-class PhoneNumber{
+class PhoneNumber {
+	async PhoneUserAdd(userID: number, PhoneNumber: string) {
+		try {
+			const addPhone = await phoneNumberModel.createPhone(userID, PhoneNumber);
 
-    async PhoneUserAdd(userID:number ,PhoneNumber: string){
-
-        try {
-
-            const addPhone = await phoneNumberModel.createPhone(userID,PhoneNumber)
-
-            if (addPhone) {
-                return{
-
-                    statusCode: 201,
-                    message: addPhone
-
-                }
-            } else {
-                return {
-                    statusCode: 400,
-                    message: message.MESSAGE_ERROR.REQUIRED_FIELDS
-                }
-            }
-        } catch (err) {
-            console.log(err);
-            return {
-              statusCode: 500,
-              message: message.MESSAGE_ERROR.INTERNAL_ERROR_DB,
-            };
-        }
-    }
+			if (addPhone) {
+				return {
+					statusCode: 201,
+					message: addPhone,
+				};
+			} else {
+				return {
+					statusCode: 400,
+					message: message.MESSAGE_ERROR.REQUIRED_FIELDS,
+				};
+			}
+		} catch (err) {
+			console.log(err);
+			return {
+				statusCode: 500,
+				message: message.MESSAGE_ERROR.INTERNAL_ERROR_DB,
+			};
+		}
+	}
 }
 
 export default new PhoneNumber();
