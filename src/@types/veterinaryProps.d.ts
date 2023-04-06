a6aa386f7af8c589e99c53731a553044f016bc75
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-04-05 22:11:06 -0300

    veterinary props

---
+++ src/@types/veterinaryProps.d.ts
@@ -0,0 +1,59 @@
+type createVeterinaryController = {
+	occupationArea: string;
+	formation: string;
+	institution: string;
+	crmv: string;
+	startActingDate: string;
+	formationDate: string;
+	animalTypes?: { id: number; name: string }[];
+	specialities?: { id: number; name: string }[];
+};
+
+type createVeterinaryModel = {
+	personName: string;
+	cpf: string;
+	email: string;
+	password: string;
+	cellphoneNumber: string;
+	phoneNumber: string | null;
+	address: {
+		zipCode: string;
+		number: string;
+		complement: string | null;
+	};
+	occupationArea: string;
+	formation: string;
+	institution: string;
+	crmv: string;
+	startActingDate: Date;
+	formationDate: Date;
+	animalTypes?: { id: number; name: string }[];
+	specialities?: { id: number; name: string }[];
+};
+
+type UpdateVeterinaryProps = {
+	personName: string;
+	cpf: string;
+	rg: string;
+	email: string;
+	password: string;
+	cellphoneNumber: string;
+	phoneNumber: string | null;
+	address: {
+		zipCode: string;
+		number: string;
+		complement: string | null;
+	};
+	occupationArea: string;
+	formation: string;
+	institution: string;
+	crmv: string;
+	animalTypes?: { id: number; name: string }[];
+	specialities?: { id: number; name: string }[];
+	bio: string;
+};
+
+type UpdateSpecialities = {
+	id: number;
+	name: string;
+};
